a3d8dd6816136a5805edd782cdeb24fa7b5bed6a
Ryan <ryan.gross.01@gmail.com>
2023-06-11 21:08:00 -0400

    Initial few shot learning test.

--- code/app.py
+++ code/app.py
@@ -2,11 +2,19 @@ import os
 import openai
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
+train1 = open("../data/fewShotTrain1.txt", "r")
+train2 = open("../data/fewShotTrain2.txt", "r")
+train3 = open("../data/fewShotTrain3.txt", "r")
+train4 = open("../data/fewShotTrain4.txt", "r")
+train5 = open("../data/fewShotTrain5.txt", "r")
+test = open("../data/fewShotTest.txt", "r")
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   temperature=0,
   messages=[
-    {"role": "user", "content": "Given: Goodbye, Say: Goodbye World, Given: Hello, Say: "},
+    {"role": "user", "content": train1.read()},
+    {"role": "user", "content": train2.read()},
+    {"role": "user", "content": test.read()},
   ]
 )
 
