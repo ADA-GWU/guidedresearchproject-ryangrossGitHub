88eaf769dd723b2fc3ce881c0b6fbffca2fd78ea
Ryan <ryan.gross.01@gmail.com>
2023-06-18 20:10:32 -0400

    Adjusted few shot training examples to be more consistent.

--- code/app.py
+++ code/app.py
@@ -4,9 +4,6 @@ openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 train1 = open("../data/fewShotTrain1.txt", "r")
 train2 = open("../data/fewShotTrain2.txt", "r")
-train3 = open("../data/fewShotTrain3.txt", "r")
-train4 = open("../data/fewShotTrain4.txt", "r")
-train5 = open("../data/fewShotTrain5.txt", "r")
 test = open("../data/fewShotTest.txt", "r")
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
@@ -18,4 +15,5 @@ completion = openai.ChatCompletion.create(
   ]
 )
 
-print(completion.choices[0].message.content)
+pred = open("../data/fewShotPred.txt", "w")
+pred.write(completion.choices[0].message.content)
