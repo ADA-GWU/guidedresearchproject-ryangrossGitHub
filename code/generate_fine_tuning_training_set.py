3c7ae4dbee3f1915f6f7d02aa4248e1a9cd2aab9
Ryan <ryan.gross.01@gmail.com>
2023-07-23 12:18:32 -0400

    Generated fine-tuning training set.

---
+++ code/generate_fine_tuning_training_set.py
@@ -0,0 +1,31 @@
+import pickle
+import numpy
+import json
+
+model = "davinci"
+
+pred_count_desired = 10 # Number of predictions you want to average for accuracy/loss metrics
+pred_hours = 3 # This can be changed to get longer predictions
+minutes_per_sample = 10 # This is not configurable, this is how the data was gathered
+sample_count = int(60/minutes_per_sample * pred_hours)
+
+training_sample_count_desired = 2 # More than 3 exceeds max prompt size
+
+fine_tune_data = []
+
+prompt = ""
+training_sample_count_actual = 0
+pred_count_actual = 0
+with open('../data/train.pkl', 'rb') as f:
+    train = pickle.load(f)
+    for i in range(len(train)):
+        traj = train[i]['traj']
+
+        if len(traj) > sample_count * 2:  # need both input and output to be the same size or else gpt response is unreliable
+            traj_slim = numpy.delete(traj, [4,5], axis=1) # Get rid of unneeded rows
+            input = traj_slim[0:sample_count]
+            output = traj_slim[sample_count:sample_count * 2]
+
+            fine_tune_data.append({"prompt": str(input.tolist()) + "\n\n###\n\n", "completion": " " + str(input.tolist()) + " ###"})
+
+open('fine_tuning.jsonl', 'w').write(json.dumps(fine_tune_data))
